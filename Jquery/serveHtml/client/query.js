224a1087b6b929e57144b98fff5f4535e778a52c
SametCimen1 <cimensamet338@gmail.com>
2021-09-30 19:34:12 -0400

    updated

--- Jquery/serveHtml/client/query.js
+++ Jquery/serveHtml/client/query.js
@@ -2,7 +2,11 @@ $(document).ready(()=>{
     $('.hello').on('click', () =>{
         // $('.secret').toggle();
         $('.secret').fadeToggle(400);
-        $('.img2').fadeToggle();
-        $('.img').fadeToggle(2000);
+        $('.img').fadeToggle(200);
+
+        $('.img2').fadeToggle(200);
+    })
+    $('.img2').on('click', () => {
+        $('.img2').slideToggle();
     })
 })
