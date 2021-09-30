7f7bb150298ed94133b93cdedf3a4ea7d2a272b8
SametCimen1 <cimensamet338@gmail.com>
2021-09-30 19:23:00 -0400

    added fadeout

--- Jquery/serveHtml/client/query.js
+++ Jquery/serveHtml/client/query.js
@@ -1,5 +1,8 @@
 $(document).ready(()=>{
     $('.hello').on('click', () =>{
-        $('.secret').toggle();
+        // $('.secret').toggle();
+        $('.secret').fadeToggle(400);
+        $('.img2').fadeToggle();
+        $('.img').fadeToggle(2000);
     })
 })
